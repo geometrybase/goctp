2a02cb58782424302efe087a5a818f21690dcba7
chenjilin <geometrybase@gmail.com>
2020-06-04 14:03:09 +0800

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
-rsync -avx ./ lgpu:/root/goctp/ --delete --progress;
-ssh lgpu "export GOPATH=/mnt/ssd/projects/go && cd /root/goctp && /usr/local/go/bin/go install -v -x -a -buildmode=shared runtime sync/atomic"
-ssh lgpu "export GOPATH=/mnt/ssd/projects/go && cd /root/goctp && /usr/local/go/bin/go install -v -x -work -buildmode=shared -linkshared"
+#rsync -avx ./ lgpu:/mnt/ssd/projects/go/src/github.com/geometrybase/goctp/ --delete --progress;
+#ssh lgpu "export GOPATH=/mnt/ssd/projects/go && cd /mnt/ssd/projects/go/src/github.com/geometrybase/goctp/ && /usr/local/go/bin/go install -v -x -a -buildmode=shared runtime sync/atomic"
+#ssh lgpu "export GOPATH=/mnt/ssd/projects/go && cd /mnt/ssd/projects/go/src/github.com/geometrybase/goctp/ && /usr/local/go/bin/go install -v -x -work -buildmode=shared -linkshared"
+git add -A
+git commit -m "update"
+git push origin master
+ssh lgpu "export GOPATH=/mnt/ssd/projects/go && /usr/local/go/bin/go get -u github.com/geometrybase/goctp"
