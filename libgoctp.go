b8819feeeca1aaceb131e78c9a3bbe91806c2b5d
chenjilin <geometrybase@gmail.com>
2020-06-05 08:10:58 +0800

    update

--- libgoctp.go
+++ libgoctp.go
@@ -7,8 +7,8 @@
 package goctp
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.3.15_20190220_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.3.15_20190220_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
-#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.3.15_20190220_api_tradeapi_se_linux64
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.3.16_20190305_tradeapi_linux64 -Wl,-rpath,${SRCDIR}/api/v6.3.16_20190305_tradeapi_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.3.16_20190305_tradeapi_linux64
 #cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows -Wl,-rpath,${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows ${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows/thostmduserapi.lib ${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows/thosttraderapi.lib -lthostmduserapi -lthosttraderapi
 #cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
 */
